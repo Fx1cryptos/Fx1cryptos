51d877699ddc732e33befdb15486a42bfe0342a2
FX1 Digital Hubs <Fxcrypto1invest@gmail.com>
2025-08-25 01:00:16 -0500

    Create microfrontend.js

---
+++ pages/microfrontend.js
@@ -0,0 +1,17 @@
+{
+  "$schema": "https://openapi.vercel.sh/microfrontends.json",
+  "applications": {
+    "fx1hubs": {
+      "development": {
+        "fallback": "fx1hubs.vercel.app"
+      }
+    },
+    "nextjs-ai-fx1hubs-th4y": {
+      "routing": [
+        {
+          "paths": [/* add paths here */]
+        }
+      ]
+    }
+  }
+}
