5bdead32e73179f79128540e4d23e2a9f167f767
FX1 Digital Hubs <Fxcrypto1invest@gmail.com>
2025-08-29 21:44:13 -0500

    Update node.js

---
+++ node.js
@@ -0,0 +1,41 @@
+{
+  "name": "fx1-digital-hubs",
+  "version": "1.0.0",
+  "description": "FX1 Digital Hubs: A Web3 platform on Base for digital creativity, NFT fashion, and $FDH token interactions.",
+  "main": "index.js",
+  "type": "module",
+  "scripts": {
+    "dev": "vite",
+    "build": "vite build",
+    "start": "node index.js",
+    "preview": "vite preview",
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "dependencies": {
+    "ethers": "^6.13.2",
+    "@solana/web3.js": "^1.95.3",
+    "dotenv": "^16.4.5"
+  },
+  "devDependencies": {
+    "vite": "^5.4.8",
+    "@vercel/node": "^3.2.7"
+  },
+  "keywords": [
+    "web3",
+    "base",
+    "nft",
+    "fashion",
+    "fdh",
+    "solana"
+  ],
+  "author": "FX1 Digital Hubs Team",
+  "license": "MIT",
+  "repository": {
+    "type": "git",
+    "url": "https://github.com/your-username/fx1-digital-hubs.git"
+  },
+  "bugs": {
+    "url": "https://github.com/your-username/fx1-digital-hubs/issues"
+  },
+  "homepage": "https://fx1-digital-hubs.vercel.app"
+}
